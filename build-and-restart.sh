67bf2faf12ce817f00df09f56cde1f98a1e3339a
daniel <daniel.strielnikow@outlook.com>
2024-12-03 14:16:12 +0100

    Dodanie bazy MySql do Dockera

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,12 +1,25 @@
 docker build -t security .
 docker stop security || true
 docker rm security || true
-docker run  \
+docker stop security-database || true
+docker rm security-database || true
+docker network rm security-network || true
+docker network create security-network
+docker run -d \
   -p 3307:3306 \
   --restart unless-stopped \
   -e MYSQL_ROOT_PASSWORD=daniel \
   -v /Users/daniel/IdeaProjects/Projects/mysql/:/var/lib/mysql \
+  --network security-network \
   --name security-database \
   mysql:latest
-docker run  -p 0.0.0.0:8080:8080 --name -e SPRING_PROFILES_ACTIVE=prod security security
+docker run -d \
+  -p 8080:8080 \
+  --name=security \
+  -e SPRING_PROFILES_ACTIVE=prod \
+  --restart unless-stopped \
+  --network security-network \
+  security
+
+
 
